834da83e80eee05b0312a11af9c769e9ac1c120e
rij6khan <rij6khan@gmail.com>
2025-12-01 13:41:33 -0500

    initial comment setup

---
+++ predict_saracasm.py
@@ -0,0 +1,28 @@
+'''-----CS461: Final Project-----
+Rija Khan (rk1047)
+Kelly Xu ()
+'''
+#importing important libraries
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+#getting info about datasets (size, label distribution, etc.)
+def getInfo(dataset):
+    pass
+
+#creating model of our choice
+
+
+
+#loading train.csv dataset for training
+
+
+#storing trained model in .pkl file
+
+
+#loading valid.csv for validation
+
+
+
+#loading test.csv for testing model
